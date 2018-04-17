331ec1082bc48bb0c53d191438d8a045c68aaf22
jamesbigenterprise <thiagoalvesda0silva@gmail.com>
2018-04-17 01:48:35 -0300

    the last

--- FinalProject/scripts/prices.js
+++ FinalProject/scripts/prices.js
@@ -6,10 +6,24 @@ Object3.onload = function()
 	var Info3 = JSON.parse(Object3.responseText);
 	console.log(Info3);
 	
-	document.getElementById('3cond').innerHTML = Info3.current_observation.weather;
-    document.getElementById('3wind').innerHTML = Info3.current_observation.wind_string;
-	document.getElementById('3img').src = Info3.current_observation.icon_url;
-	document.getElementById('3temp').innerHTML = Info3.current_observation.temperature_string;
+	document.getElementById('s1').innerHTML = Info3.services["0"].name;
+	document.getElementById('p1').innerHTML= Info3.services["0"].price;
+
+	document.getElementById('s2').innerHTML = Info3.services["1"].name;
+	document.getElementById('p2').innerHTML= Info3.services["1"].price;
+
+	document.getElementById('s3').innerHTML = Info3.services["2"].name;
+	document.getElementById('p3').innerHTML= Info3.services["2"].price;
+
+	document.getElementById('s4').innerHTML = Info3.services["3"].name;
+	document.getElementById('p4').innerHTML= Info3.services["3"].price;
+
+	document.getElementById('s5').innerHTML = Info3.services["4"].name;
+	document.getElementById('p5').innerHTML= Info3.services["4"].price;
+
+	document.getElementById('s6').innerHTML = Info3.services["5"].name;
+	document.getElementById('p6').innerHTML= Info3.services["5"].price;
+
 	
 } // end of onload
 
