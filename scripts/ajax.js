277f80b7c3e7ad98aca90966e891eac4091f8e54
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-08 20:37:15 -0300

    Update ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -7,9 +7,8 @@ function getCities(country)
 	        //document.getElementById("input").innerHTML = xht.responseText;
 			var cities = xht.responseText.split("\n");
 			console.log(cities[0]);
-            var i;
             var  table ="<tr><th>City</th><th>Population</th></tr>";
-            for (i = 0; i < cities.length; i++)
+            for (i = 0; i < cities.length; ++i)
             {
                table += "<tr><td>" +
                cities[i] +	"</td><td>" + cities[i++] + "</td></tr>";		   
