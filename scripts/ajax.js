e48eb452f394907091e167c772ce1bf24f3da585
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-08 21:20:29 -0300

    rrr

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -5,15 +5,11 @@ function getCities(country)
 	     if(this.readyState == 4 && this.status == 200)
 	     {
 	        //document.getElementById("input").innerHTML = xht.responseText;
-			var cities = xht.responseText.split("\n");
+			var cities = xht.responseText;//.split("\n");
 		
 			console.log(cities[0]);
-            var  table ="<tr><th>City</th><th>Population</th></tr>";
-            for (i = 0; i < cities.length; i++)
-            {
-               table += "<tr><td>" +
-               cities[i] +	"</td><td>" + cities[++i] + "</td></tr>";		   
-            }
+            var  table ="<pre>" + cities + "</pre>";
+            
 			document.getElementById("input").innerHTML = table;
           }
 	     }
