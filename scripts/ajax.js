f8626ab0cd78fe9f44d4891dde6bc1e684c7e42f
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-09 19:46:18 -0300

    Update ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -24,6 +24,7 @@ function getStudents()
 	     {
 			var json = xht.responseText;
 			var students = JSON.parse(json);
+			console.log(students[i].first);
 			console.log(students.length + "LENGTH");
 
             var  table = "<tr><th>Name</th><th>Adress</th><th>Major</th><th>GPA</th></tr>";
