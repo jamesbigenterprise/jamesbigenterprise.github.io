f3c39038877d46d262cb02945b051564fe3f8c24
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-08 21:31:17 -0300

    eee

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -5,7 +5,7 @@ function getCities(country)
 	     if(this.readyState == 4 && this.status == 200)
 	     {
 	        //document.getElementById("input").innerHTML = xht.responseText;
-			var cities = xht.responseText;//.split("\n");
+			var cities = xht.responseText;
 		
 			console.log(cities[0]);
             var  table ="<pre>" + cities + "</pre>";
@@ -14,6 +14,27 @@ function getCities(country)
           }
 	     }
 	   xht.open("GET", country, true);
+	   xht.send();
+	   
+	   console.log(country);
+	} 
+	
+function getStudents(file)
+	{
+	   var xht = new XMLHttpRequest();
+	   xht.onreadystatechange = function(){
+	     if(this.readyState == 4 && this.status == 200)
+	     {
+	        //document.getElementById("input").innerHTML = xht.responseText;
+			var students = JSON.parse(xht.responseText);
+		
+			console.log(students[0]);
+            var  outHtml ="<pre>" + cities + "</pre>";
+            
+			document.getElementById("jsonout").innerHTML = outHtml;
+          }
+	     }
+	   xht.open("GET", file, true);
 	   xht.send();
 	   
 	   console.log(country);
