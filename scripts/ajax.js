730edb78abd47a04eafc19a95cb3cb40ed66f8ee
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-09 21:50:33 -0300

    Update ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -17,8 +17,45 @@ function getCities(country)
 	
 function getStudents()
 {
-   var xmlhttp = new XMLHttpRequest();
-   var url = "json.txt";
+	var fileName = document.getElementById("file").value;
+	   var xht = new XMLHttpRequest();
+	   xht.onreadystatechange = function(){
+	     if(this.readyState == 4 && this.status == 200)
+	     {
+			var json = xht.responseText;
+			var students = JSON.parse(json);
+			
+			var second = JSON.parse(xht.responseText);
+			console.log(second[0]);
+			printJson(second);
+          }
+		  if(this.readyState == 4 && this.status == 404)
+		  {
+		    document.getElementById("jsonout").innerHTML = "<th> File " + fileName + " not found</th>";   
+		  }
+	     }
+	   xht.open("GET", fileName, true);
+	   xht.send();
+	}
+	
+function printJson(data)
+{
+	
+   var  table = "<tr><th>Name</th><th>Adress</th><th>Major</th><th>GPA</th></tr>";
+   var i;
+   for (i=0; i < data.length; i++)
+	{
+	  table += "<tr><td>" + data[i].first + " " + data[i].last + "</td><td>" + 
+	  data[i].address.city + "-" + data[i].address.state + " " + 
+	  data[i].address.zip + "</td><td>" + data[i].major + "</td><td>" + 
+	  data[i].gpa + "</td></tr>";
+	}
+	document.getElementById("jsonout").innerHTML = table;
+}		
+
+
+var xmlhttp = new XMLHttpRequest();
+var url = "myTutorials.txt";
 
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -33,14 +70,8 @@ function myFunction(arr) {
   var out = "";
   var i;
   for(i = 0; i < arr.length; i++) {
-    out += '<p"' + arr[i].first + '">' + 
-    arr[i].first + '</p><br>';
+    out += '<a href="' + arr[i].url + '">' + 
+    arr[i].display + '</a><br>';
   }
-  document.getElementById("jsonout").innerHTML = out;
-}	
-console.log("Over here");
-console.log(myArr[0].first);
-}		
-
-
-
+  document.getElementById("id01").innerHTML = out;
+}
