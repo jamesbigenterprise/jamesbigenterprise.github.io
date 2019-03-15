6608270b0a4a94410bf2693508a525c88d617a68
jamesenterprise <thiagoalvesda0silva@gmail.com>
2019-03-15 13:39:56 -0300

    Update mortgage.php

--- W10/mortgage.php
+++ W10/mortgage.php
@@ -1,12 +1,23 @@
-<?php
-   $apr = $_REQUEST['APR'];
-   $term = $_REQUEST['term'];
-   $amount =$_REQUEST['amount'];
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+
+    <title>Mortgage Calculator - PHP</title>
+  </head>
+  <body>
+  
+  <?php
+    $apr = $_REQUEST['APR'];
+    $term = $_REQUEST['term'];
+    $amount =$_REQUEST['amount'];
    
-   echo "<!DOCTYPE html> <html lang="en-us">";
-   echo("APR = " . $apr . "<br/>");
-   echo("TERM = " . $term . "<br/>");
-   echo("AMOUUNT = " . $amount . "<br/>");
-   echo "</html>";
+    print "<h1>Hello world from PHP</h1>";
+    /*echo("APR = " . $apr . "<br/>");
+    echo("TERM = " . $term . "<br/>");
+    echo("AMOUUNT = " . $amount . "<br/>");
+    echo "</html>";
+	*/
    
-?>
+  ?>
+  </body>
+</html>
